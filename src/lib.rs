0cab23219106610da00e14ebf6d23326911f0017
Samuel Schlesinger <samuelschlesinger@Samuels-MacBook-Air.local>
2023-04-19 03:52:45 -0400

    fixed docs up a bit

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,7 @@
+/// An implementation of Lamport signatures
 pub mod lamport;
+/// Builds off of the Lamport signatures by implementing a
+/// Merkle commitment to a vector of Lamport public keys,
+/// with a Merkle proof coupled with a Lamport signature
+/// comprising a signature
 pub mod merkle;
