8684d7cd24f445371031f2c778f8d881236bd174
Samuel Schlesinger <samuelschlesinger@Samuels-MacBook-Air.local>
2023-04-21 05:05:00 -0400

    added enough functionality to encode/decode these things from files

--- src/merkle.rs
+++ src/merkle.rs
@@ -16,19 +16,8 @@ impl PublicKey {
 /// of Lamport public keys, one for each message you plan to sign.
 pub struct PrivateKey(Vec<lamport::PrivateKey>, Tree, usize);
 
-/// A signature consists of a lamport signature and a merkle proof of the
-/// public key used.
-pub struct Signature(lamport::Signature, lamport::PublicKey, Proof);
-
-impl PrivateKey {
-    pub fn public_key(&self) -> PublicKey {
-        PublicKey(self.1.commitment())
-    }
-
-    pub fn generate(n: usize) -> Result<PrivateKey, rand::Error> {
-        let private_keys: Result<Vec<lamport::PrivateKey>, rand::Error> =
-            (0..n).map(|_i| lamport::PrivateKey::generate()).collect();
-        let private_keys = private_keys?;
+impl From<Vec<lamport::PrivateKey>> for PrivateKey {
+    fn from(private_keys: Vec<lamport::PrivateKey>) -> Self {
         let encoded_public_keys: Vec<Vec<u8>> = private_keys
             .iter()
             .map(|private_key| {
@@ -41,7 +30,28 @@ impl PrivateKey {
             })
             .collect();
         let tree = Tree::new(&mut encoded_public_keys.iter().map(|v| v.as_slice()));
-        Ok(PrivateKey(private_keys, tree, 0))
+        PrivateKey(private_keys, tree, 0)
+    }
+}
+
+/// A signature consists of a lamport signature and a merkle proof of the
+/// public key used.
+pub struct Signature(lamport::Signature, lamport::PublicKey, Proof);
+
+impl PrivateKey {
+    pub fn inner_keys(&self) -> &Vec<lamport::PrivateKey> {
+        &self.0
+    }
+
+    pub fn public_key(&self) -> PublicKey {
+        PublicKey(self.1.commitment())
+    }
+
+    pub fn generate(n: usize) -> Result<PrivateKey, rand::Error> {
+        let private_keys: Result<Vec<lamport::PrivateKey>, rand::Error> =
+            (0..n).map(|_i| lamport::PrivateKey::generate()).collect();
+        let private_keys = private_keys?;
+        Ok(private_keys.into())
     }
 
     pub fn sign<A: AsRef<[u8]>>(&mut self, message: A) -> Option<Signature> {
